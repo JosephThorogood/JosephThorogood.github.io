0b9de92d547b7c289066fc8ba1db0f9a48ea73ce
JosephThorogood <38864296+JosephThorogood@users.noreply.github.com>
2022-02-16 12:40:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 (function() {
   const wheel = document.querySelector('.wheel');
-  const startButton = document.querySelector('.wheel');
+  const startButton = document.querySelector('.button');
   const display = document.querySelector('.display');
   
   let deg = 0;
